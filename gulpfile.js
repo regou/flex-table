1e67616dcca2c33206833974e07fa150868dc753
xiaosee@ssl-mail.com <036098KIll>
2015-02-19 14:55:19 +0800

    gulp init

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+/* gulpfile.js */
+
+// Load some modules which are installed through NPM.
+var gulp = require('gulp');
+var browserify = require('browserify');  // Bundles JS.
+var del = require('del');  // Deletes files.
+var reactify = require('reactify');  // Transforms React JSX to JS.
+var source = require('vinyl-source-stream');
+
+// Define some paths.
+var paths = {
+    app_js: ['./js/index.js'],
+    js: ['./js/**/*.js*']
+};
+
+
+
+// Our JS task. It will Browserify our code and compile React JSX files.
+gulp.task('js', function() {
+    // Browserify/bundle the JS.
+    browserify({
+        entries:paths.app_js,
+        debug:true
+    })
+        .transform(reactify)
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./compiles/'));
+});
+
+// Rerun tasks whenever a file changes.
+gulp.task('watch', function() {
+    gulp.watch(paths.js, ['js']);
+});
+
+// The default task (called when we run `gulp` from cli)
+gulp.task('default', ['watch', 'js']);
---
+++ js/index.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+
+var Taz = require('./jsx/Taz.jsx');
+
+   window.qqaa=1500000;
+
+
+
+
+React.render(<Taz/>,document.body);
