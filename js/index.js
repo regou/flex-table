a6e1479698415e969fbf56fd2939045f586de03e
xiaosee@ssl-mail.com <036098KIll>
2015-02-20 22:39:28 +0800

    numComa

--- js/index.js
+++ js/index.js
@@ -10,70 +10,70 @@ var tableData = {
    "tbody": [
       [
          "CN1476182A",
-         75,
+         1714,
          "Method of radio chain circuit control resetting or treatment of timer after reconstitution in radio communication system",
          "2003-03-20",
          "2004-02-18"
       ],
       [
          "CN1761260A",
-         64,
+         3012,
          "Method for preventing deadlock in a wireless communications system and radio plant",
          "2005-09-15",
          "2006-04-19"
       ],
       [
          "CN1937627A",
-         12,
+         1223,
          "Method and apparatus fo handling timers during reestablishing transmitting sides in wireless communications systems",
          "2006-09-20",
          "2007-03-28"
       ],
       [
          "CN101272619A",
-         37,
+         1076,
          "Method for enhancing data transmission efficiency for a radio resource control procedure",
          "2008-03-21",
          "2008-09-24"
       ],
       [
          "CN101267286A",
-         36,
+         2333,
          "Method and apparatus for improving MIMO function in a wireless communications system",
          "2008-03-14",
          "2008-09-17"
       ],
       [
          "CN101325477A",
-         3,
+         323,
          "Method and apparatus for improving drx operation in a wireless communications system",
          "2008-06-13",
          "2008-12-17"
       ],
       [
          "CN101188494A",
-         32,
+         145,
          "Method and apparatus for configuring signaling radio bearer in a wireless communications system",
          "2007-11-21",
          "2008-05-28"
       ],
       [
          "CN101272561A",
-         32,
+         2100,
          "Method and apparatus for handling random access procedure in a wireless communications system",
          "2008-03-21",
          "2008-09-24"
       ],
       [
          "CN101184269A",
-         10,
+         1023,
          "Method and apparatus of selecting operating frequency for user equipment in a wireless communications system",
          "2007-11-13",
          "2008-05-21"
       ],
       [
          "CN101232657A",
-         5,
+         53,
          "Method and related apparatus for improving MIMO function in a wireless communication system",
          "2008-01-22",
          "2008-07-30"
